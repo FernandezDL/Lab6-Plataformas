09fc7512c80d84dfeb5c7d2134150a9907337844
FernandezDL <fer21747@uvg.edu.gt>
2022-08-21 23:24:01 -0600

    avances en el proyecto, parte 1

--- app/src/main/java/com/diana/lab6/MainActivity.kt
+++ app/src/main/java/com/diana/lab6/MainActivity.kt
@@ -2,10 +2,30 @@ package com.diana.lab6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import androidx.fragment.app.Fragment
+//import androidx.fragment.app.commit
+//import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    //private lateinit var navBttn:BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //navBttn= findViewById(R.layout.)
+        //setListeners()
     }
+
+    //private fun setListeners(){
+
+   // }
+
+    //private fun setFragment(fragment: Fragment){
+      //  supportFragmentManager.commit {
+           // setReorderingAllowed(true)
+
+       // }
+   // }
 }
