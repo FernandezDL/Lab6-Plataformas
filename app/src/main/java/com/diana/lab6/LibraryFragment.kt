de8005e01701f07a44d1ae1e7e5c90f70ec2bd69
FernandezDL <fer21747@uvg.edu.gt>
2022-08-22 16:46:54 -0600

    Terminado

--- app/src/main/java/com/diana/lab6/LibraryFragment.kt
+++ app/src/main/java/com/diana/lab6/LibraryFragment.kt
@@ -2,17 +2,29 @@ package com.diana.lab6
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 
 class LibraryFragment : Fragment(R.layout.fragment_library) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false)
+    private lateinit var bttnCount: ImageButton
+    private lateinit var txt: TextView
+    private var counter = 0
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bttnCount = view.findViewById(R.id.add)
+        txt = view.findViewById(R.id.num)
+
+        setListeners()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setListeners(){
+        bttnCount.setOnClickListener {
+            counter++
+            txt.setText(counter.toString())
+        }
     }
 }
--- app/src/main/java/com/diana/lab6/MainActivity.kt
+++ app/src/main/java/com/diana/lab6/MainActivity.kt
@@ -2,30 +2,35 @@ package com.diana.lab6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import androidx.fragment.app.Fragment
-//import androidx.fragment.app.commit
-//import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    //private lateinit var navBttn:BottomNavigationView
-
+    private lateinit var navBttn:BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //navBttn= findViewById(R.layout.)
-        //setListeners()
+        navBttn= findViewById(R.id.Menu)
+        setListeners()
     }
 
-    //private fun setListeners(){
+    private fun setListeners(){
+        navBttn.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.homeMenu -> setFragment(HomeFragment())
+                R.id.menuLibreria -> setFragment(LibraryFragment())
+                R.id.searchMenu -> setFragment(serchFragment())
+            }
+            true
+        }
+    }
 
-   // }
-
-    //private fun setFragment(fragment: Fragment){
-      //  supportFragmentManager.commit {
-           // setReorderingAllowed(true)
-
-       // }
-   // }
+    private fun setFragment(fragment: Fragment){
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.Container1, fragment)
+        }
+    }
 }
